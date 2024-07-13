bb6937d9f9311b1be740c3c3c83ee572f57484b9
dilanchoksi <dilanchoksi@gmail.com>
2024-07-13 18:34:58 -0400

    prod

---
+++ src/components/Contact.jsx
@@ -0,0 +1,38 @@
+import { CONTACT } from "../constants";
+import { motion } from "framer-motion";
+
+const Contact = () => {
+  return (
+    <div className="border b border-neutral-900 pb-20">
+      <motion.h1
+        whileInView={{ opacity: 1, y: 0 }}
+        initial={{ opacity: 0, y: -100 }}
+        transition={{ duration: 0.5 }}
+        className="my-10 text-center text-4xl"
+      >
+        Get in Touch
+      </motion.h1>
+      <div className="text-center tracking-tighter">
+        <motion.a
+          whileInView={{ opacity: 1, x: 0 }}
+          initial={{ opacity: 0, x: -100 }}
+          transition={{ duration: 1 }}
+          href="#"
+          className="my-4"
+        >
+          {CONTACT.email}
+        </motion.a>
+        <motion.p
+          whileInView={{ opacity: 1, x: 0 }}
+          initial={{ opacity: 0, x: 100 }}
+          transition={{ duration: 1 }}
+          className="my-4"
+        >
+          {CONTACT.phoneNo}
+        </motion.p>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import logo from "../assets/kevinRushLogo.png";
+import logo from "../assets/logo.png";
 import { FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
 import { FaSquareXTwitter } from "react-icons/fa6";
@@ -6,9 +6,9 @@ import { FaInstagram } from "react-icons/fa";
 
 const Navbar = () => {
   return (
-    <nav className="bg-red-900 mb-20 flex items-center justify-between py-6">
+    <nav className="mb-20 flex items-center justify-between py-6">
       <div className="flex flex-shrink-0 items-center">
-        <img src={logo} alt="" />
+        <img className="mx-2 w-28" src={logo} alt="" />
       </div>
       <div className="m-8 flex items-center justify-center gap-4 text-2xl">
         <FaLinkedin />
