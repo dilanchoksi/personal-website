ac8c31b1367f28275c671491d951c937e5150783
Dilan N Choksi <dilan.choksi@wakefern.com>
2024-07-13 12:33:48 -0400

    test images

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import Navbar from "./components/Navbar";
+
 const App = () => {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
+      <Navbar />
     </div>
   );
 };
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import logo from "../assets/kevinRushLogo.png";
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { FaSquareXTwitter } from "react-icons/fa6";
+import { FaInstagram } from "react-icons/fa";
+
+const Navbar = () => {
+  return (
+    <nav className="bg-red-900 mb-20 flex items-center justify-between py-6">
+      <div className="flex flex-shrink-0 items-center">
+        <img src={logo} alt="" />
+      </div>
+      <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+        <FaLinkedin />
+        <FaGithub />
+        <FaSquareXTwitter />
+        <FaInstagram />
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
