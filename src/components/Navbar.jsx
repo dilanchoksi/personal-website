6e5e8def33e4ba7a783d7a21fd114f1e98b3be86
dilanchoksi <dilanchoksi@gmail.com>
2024-07-16 20:54:40 -0400

    removed previous collapsable navbar -- too clunky

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -31,92 +31,69 @@ const Navbar = () => {
   };
 
   return (
-    <>
-      <div
-        className={`inline-block fixed -ml-7 z-50 duration-1000 ${
-          scrolled ? "md:pt-3 py-2.5" : "py-10"
-        }`}
-      >
+    <nav
+      className={`flex items-center justify-between fixed w-full top-0 left-0 md:px-6 lg:px-16 px-24 z-40 __cust_trans duration-1000 ${
+        scrolled ? "bg-zinc-300 opacity-95 py-2" : "bg-transparent py-10"
+      }`}
+    >
+      <div className="flex flex-shrink-0 items-center z-50">
         <img
-          className={`w-28 md:mx-2 md:w-28 md:duration-700 ${
-            scrolled ? "invert" : ""
-          }`}
+          className={`mx-2 w-28 duration-700 ${scrolled ? "invert" : ""}`}
           src={logo}
           alt=""
         />
       </div>
-      <div
-        className={`inline-block py-auto text-4xl md:hidden fixed left-0 w-screen z-40 duration-1000 h-auto ${
-          scrolled ? "bg-white py-2 text-black" : "py-10"
-        } `}
-      >
-        <button onClick={toggleMenu} className="float-right mr-6">
-          <FaBars className=""></FaBars>
-        </button>
-      </div>
-      <nav
-        className={`duration-500 bg-zinc-300 h-screen top-0 p-10 flex fixed flex-col gap-4 z-50 w-1/2 md:justify-end md:h-auto md:right-0 md:flex md:flex-row md:gap-4 md:items-center md:fixed md:w-full md:top-0 md:left-0 md:px-6 md:z-40 md:duration-1000 md:bg-transparent md:py-10 ${
-          scrolled ? "md:bg-zinc-300 md:opacity-95 md:py-2" : ""
-        } ${menuOpen ? "left-1/2" : "left-full"}`}
-      >
-        <button
-          onClick={toggleMenu}
-          className="absolute text-4xl right-4 top-4 md:hidden text-black"
+      <div className="flex gap-4 z-50">
+        <ul
+          className={`flex items-center justify-center gap-4 font-sans font-bold duration-1000 ${
+            scrolled ? "text-black" : ""
+          }`}
         >
-          <FaTimes></FaTimes>
-        </button>
-        <div className="flex flex-col gap-6 text-3xl md:flex-row md:gap-4 md:z-50">
-          <ul
-            className={`text-black gap-6 flex-col flex font-bold md:text-white md:flex-row md:items-center md:justify-center md:gap-4 md:font-normal md:font-sans md:duration-1000 ${
-              scrolled ? "md:invert" : ""
-            }`}
+          <li>
+            <a href="#about">ABOUT</a>
+          </li>
+          <li>
+            <a href="#skills">SKILLS</a>
+          </li>
+          <li>
+            <a href="#experience">EXPERIENCE</a>
+          </li>
+          <li>
+            <a href="#projects">PROJECTS</a>
+          </li>
+          <li>
+            <a href="#contact">CONTACT</a>
+          </li>
+        </ul>
+        <div
+          className={`flex items-center justify-center gap-4 text-lg rounded-full p-1.5 z-50 duration-1000 ${
+            scrolled ? "bg-black text-white" : "bg-white text-black"
+          }`}
+        >
+          <a
+            href="https://www.linkedin.com/in/dilanchoksi/"
+            className="hover:text-[#0a66c2]"
+            target="_blank"
           >
-            <li>
-              <a href="#about">ABOUT</a>
-            </li>
-            <li>
-              <a href="#skills">SKILLS</a>
-            </li>
-            <li>
-              <a href="#experience">EXPERIENCE</a>
-            </li>
-            <li>
-              <a href="#projects">PROJECTS</a>
-            </li>
-            <li>
-              <a href="#contact">CONTACT</a>
-            </li>
-          </ul>
-          <div
-            className={`text-white bg-black flex items-center justify-center gap-4 text-lg rounded-full w-24 p-1.5 z-50 duration-1000 md:bg-white md:text-black ${
-              scrolled ? "md:bg-black md:text-white" : ""
-            }`}
+            <FaLinkedin />
+          </a>
+          <a
+            href="https://github.com/dilanchoksi/"
+            className="hover:text-[#7C007C]"
+            target="_blank"
           >
-            <a
-              href="https://www.linkedin.com/in/dilanchoksi/"
-              className="hover:text-[#0a66c2]"
-              target="_blank"
-            >
-              <FaLinkedin />
-            </a>
-            <a
-              href="https://github.com/dilanchoksi/"
-              className="hover:text-[#7C007C]"
-              target="_blank"
-            >
-              <FaGithub />
-            </a>
-            <a
-              href="https://instagram.com/dilanchoksi_/"
-              className="hover:text-[#D62976]"
-              target="_blank"
-            >
-              <FaInstagram />
-            </a>
-          </div>
+            <FaGithub />
+          </a>
+          <a
+            href="https://instagram.com/dilanchoksi_/"
+            className="hover:text-[#D62976]"
+            target="_blank"
+          >
+            <FaInstagram />
+          </a>
         </div>
-      </nav>
-    </>
+      </div>
+    </nav>
   );
 };
 
