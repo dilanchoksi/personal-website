c631a1b2715eb61f027aab6b509a36d604fce23b
Dilan N Choksi <dilan.choksi@wakefern.com>
2024-07-17 11:40:19 -0400

    fixed compact menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -33,14 +33,12 @@ const Navbar = () => {
   return (
     <nav
       className={`flex items-center z-40 justify-between fixed w-full top-0 left-0 px-6 lg:px-16 xl:px-24 duration-1000 ${
-        scrolled || menuOpen ? "bg-zinc-300 py-2" : "bg-transparent py-10"
+        scrolled ? "bg-zinc-300 py-2" : "bg-transparent py-10"
       }`}
     >
       <div className="flex flex-shrink-0 items-center">
         <img
-          className={`mx-2 h-7 duration-700 ${
-            scrolled || menuOpen ? "invert" : ""
-          }`}
+          className={`mx-2 h-7 duration-700 ${scrolled ? "invert" : ""}`}
           src={logo}
           alt=""
         />
@@ -48,24 +46,30 @@ const Navbar = () => {
       <button>
         <FaBars
           onClick={toggleMenu}
-          className={`relative z-50 ${
-            menuOpen ? "hidden" : `${scrolled ? "text-black" : "text-white"}`
-          } `}
+          className={`md:hidden relative z-50 ${menuOpen ? "hidden" : ""} ${
+            scrolled ? "text-black" : "text-white"
+          }`}
         ></FaBars>
         <FaTimes
           onClick={toggleMenu}
-          className={`relative z-50 ${menuOpen ? "text-black" : "hidden"} `}
+          className={`md:hidden relative z-50 ${menuOpen ? "" : "hidden"} ${
+            scrolled ? "text-black" : "text-white"
+          }`}
         ></FaTimes>
       </button>
 
       <div
-        className={` flex sm:top-11 gap-4 bg-zinc-300 sm:fixed sm:flex-col sm:w-screen sm:left-0 sm:items-center py-4 duration-500 ${
-          menuOpen ? "opacity-95" : "opacity-0"
-        }`}
+        className={`border-y-2 md:border-none border-white flex gap-4 fixed md:static flex-col md:flex-row flex-shrink-0 w-screen md:w-auto left-0 items-center py-4 md:py-auto duration-1000 md:opacity-100 ${
+          menuOpen ? "opacity-[.97]" : "opacity-0"
+        } ${
+          scrolled
+            ? "top-11 bg-zinc-300 md:bg-transparent"
+            : "top-[108px] text-white bg-black md:bg-transparent"
+        } `}
       >
         <ul
-          className={`flex items-center justify-center gap-0 font-sans font-bold duration-1000 sm:flex-col sm:gap-4 ${
-            scrolled || menuOpen ? "text-black" : ""
+          className={`flex items-center justify-center gap-0 font-sans font-bold duration-1000 flex-col md:flex-row md:gap-4 ${
+            scrolled ? "text-black" : ""
           }`}
         >
           <li>
