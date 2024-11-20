7093f64e2118da7041985c1ae2a382d868511f8d
dilanchoksi <dilanchoksi@gmail.com>
2024-11-20 01:08:34 -0600

    text tweaks

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -14,17 +14,17 @@ export const NAVBAR_TABS = [
 export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. With 5 years of hands-on experience, I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js, MySQL, PostgreSQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
 
 export const ABOUT_TEXT =
-  "Hi! I am an undergraduate student studying Computer Science and Statistics at the University of Illinois Urbana-Champaign. As a versatile full-stack-developer, I have a passion for creating efficient and user-friendly web applications. I have worked with a variety of languages and technologies including Java, Python, JavaScript, Node.js, React.js, MongoDB, and more.  My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active with sports, playing poker, and exploring new technologies.";
+  "Hi! I'm an undergraduate student studying Computer Science, Statistics, and Data Science at the University of Illinois Urbana-Champaign. As a versatile full-stack-developer, I have a passion for creating socially impactful and user-friendly web applications. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I've worked with a variety of languages and technologies including Java (Spring Boot), Python (Django, Flask), JavaScript (React, Node.js, Next.js), and more. Outside of coding, I enjoy staying active with sports, playing poker, and exploring new technologies.";
 
 export const SKILLS = [
-  { language: "Java", frameworks: ["Spring Boot", "Java EE", "Jackson"] },
+  { language: "Java", frameworks: ["Spring Boot", "Java EE"] },
   {
     language: "Python",
-    frameworks: ["Pandas", "Scikit-learn", "NumPy"],
+    frameworks: ["Django", "Flask", "Pandas", "Scikit-learn"],
   },
   {
     language: "JavaScript",
-    frameworks: ["Node.js", "Express.js", "React.js", "Next.js", "jQuery"],
+    frameworks: ["React", "Next.js", "Node.js", "Express"],
   },
   {
     language: "C++",
@@ -32,7 +32,7 @@ export const SKILLS = [
   },
   {
     language: "HTML/CSS",
-    frameworks: ["Bootstrap", "Tailwind"],
+    frameworks: ["Tailwind", "Bootstrap"],
   },
   {
     language: "SQL/MQL",
@@ -71,7 +71,7 @@ export const EXPERIENCES = [
       "Axios",
       "Prometheus",
       "Apache HTTP",
-      "Podman",
+      "Docker",
       "HTML/CSS",
     ],
   },
@@ -80,14 +80,14 @@ export const EXPERIENCES = [
     role: "Software Developer",
     company: "Project Code",
     description: `Contributed to the development an apartment finder application for students.`,
-    technologies: ["Python", "Scikit-learn", "Pandas"],
+    technologies: ["Node.js", "Express", "MongoDB", "Redis", "Socket.io"],
   },
   {
     year: "February 2022 \u2192 November 2022",
     role: "Code Instructor",
     company: "Code Ninjas",
     description: `Taught various applications of JavaScript, Java, and Python to students, maintaining a project-based learning environment.`,
-    technologies: ["Node.js", "Express.js", "MongoDB", "Axios", "WebSockets"],
+    technologies: ["Python", "Java", "JavaScript"],
   },
   {
     year: "September 2021 \u2192 June 2022",
@@ -100,7 +100,7 @@ export const EXPERIENCES = [
 
 export const PROJECTS = [
   {
-    title: "InterView AI",
+    title: "InterView",
     image: project1,
     description: "An AI-powered mock interview simulator.",
     technologies: [
@@ -119,13 +119,7 @@ export const PROJECTS = [
     title: "MoneyMap",
     image: project2,
     description: "A full-stack expense tracker built with MERN.",
-    technologies: [
-      "React.js",
-      "Node.js",
-      "Express.js",
-      "MongoDB",
-      "Clerk Authentication",
-    ],
+    technologies: ["React.js", "Node.js", "Express.js", "MongoDB", "Clerk"],
     web: "https://money-map-red.onrender.com",
     gh: "https://github.com/dilanchoksi/money-map",
   },
@@ -133,16 +127,15 @@ export const PROJECTS = [
     title: "DiamondAlysis",
     image: project3,
     description:
-      "A research project using exploratory data analysis and machine learning to explore the valuation metrics of diamonds.",
-    technologies: ["Python", "Pandas", "Matplotlib", "Scikit-learn"],
+      "A research project using EDA and machine learning to explore the valuation metrics of diamonds.",
+    technologies: ["Python", "Pandas", "Scikit-learn"],
     gh: "https://github.com/dilanchoksi/diamondalysis",
   },
   {
     title: "Personal Website",
     image: project4,
-    description:
-      "The website you're looking at! Built to showcase my professional life.",
-    technologies: ["React.js", "Tailwind", "Framer Motion", "HTML/CSS"],
+    description: "Built to showcase my professional life (this website).",
+    technologies: ["React.js", "Tailwind", "Framer Motion"],
     gh: "https://github.com/dilanchoksi/personal-website",
   },
 ];
